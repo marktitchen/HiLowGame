b4cc8d3d65f8f83bddb2bb2eff73bfc4c798d30d
Mark Titchen <46524303+marktitchen@users.noreply.github.com>
2021-03-31 11:42:57 +0100

    Add files via upload

---
+++ HiLowGame/HiLowGame/arrayHiLow.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "test array";
+
+	return 0;
+}
+
